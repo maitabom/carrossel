4d06ae1d3ed647e618a7fc8118dfdf32028ca512
Fábio Valentim <maitabom@gmail.com>
2019-07-27 20:29:54 -0300

    Implantação do script para efeito loading de barras do carrossel

---
+++ script.js
@@ -0,0 +1,7 @@
+$(function () {
+    /*$('#carousel').on('slid.bs.carousel', function () {
+        $('.carousel-indicators li').animate({
+            width: '100%'
+        }, 3000);
+    });*/
+});

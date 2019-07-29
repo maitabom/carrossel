7a97fd10f48a350d5e49f04d7de583d2eccd1467
Fábio Valentim <maitabom@gmail.com>
2019-07-29 14:38:13 -0300

    Implantação de script de controle e barra de indicação

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 $(function () {
-    /*$('#carousel').on('slid.bs.carousel', function () {
-        $('.carousel-indicators li').animate({
-            width: '100%'
-        }, 3000);
-    });*/
+    $('#carousel').on('slid.bs.carousel', function (e) {
+        if(e.to == 0) {
+            $('.carousel-indicators .progress-bar').css('width', '0%');
+        }
+        animarProgress(); 
+    });
+
+    animarProgress();
 });
+
+function animarProgress() {
+    $('.carousel-indicators li.active .progress-bar').animate({
+        width: '100%'
+    }, 2250);
+}
